374932f93030decba757a6d2ba2fd4a18c70c4ed
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2022-12-29 12:03:20 -0600

    Anagram version 1 compuesta en java y kotlin

--- src/main/java/ArraysHash/E2Anagram.java
+++ src/main/java/ArraysHash/E2Anagram.java
@@ -1,5 +1,75 @@
 package ArraysHash;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class E2Anagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        for (int i = 0; i < sc.length; i++) {
+            if (sc[i] != tc[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isAnagram2(String s, String t) {
+
+        HashMap<Character, Integer> hs = new HashMap<>();
+
+        if (s.length() != t.length()) return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hs.containsKey(s.charAt(i))) {
+                hs.put(s.charAt(i), hs.get(s.charAt(i)) + 1);
+            } else {
+                hs.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            if (hs.containsKey(t.charAt(j)) && hs.get(t.charAt(j)) > 0) {
+                hs.put(t.charAt(j), hs.get(t.charAt(j)) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAnagram3(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        int[] alphabet = new int[26]; // a b c d f g h i j k ...
+
+        // anagram
+
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a']++;
+            alphabet[t.charAt(i) - 'a']--;
+        }
+
+        for (int letter : alphabet) {
+            if (letter != 0) return false;
+        }
+
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        E2Anagram a = new E2Anagram();
+        a.isAnagram3("anagram", "anagram");
+    }
 
 }
--- src/main/kotlin/E2Anagram.kt
+++ src/main/kotlin/E2Anagram.kt
@@ -1,2 +1,16 @@
-class E2Anagram {
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false;
+
+    val sc = s.toCharArray()
+    val tc = t.toCharArray()
+
+    sc.sort()
+    tc.sort()
+
+    for (i in sc.indices) {
+        if (sc[i] != tc[i]) {
+            return false
+        }
+    }
+    return true
 }
