6a51af6316198c5f0fe12c441285a8d350f14b43
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2023-01-11 10:31:10 -0600

    anagram group

---
+++ src/main/java/ArraysHash/E4GroupAnagrams.java
@@ -0,0 +1,22 @@
+package ArraysHash;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class E4GroupAnagrams {
+
+    public static void main(String[] args) {
+        E4GroupAnagrams o = new E4GroupAnagrams();
+        o.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        for (String s : strs) {
+            int[] count = new int[26];
+        }
+
+        return new ArrayList<>();
+    }
+
+}
